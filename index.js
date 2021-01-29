91ea1015ed65fa0c964805cef256904356a1882f
vidigummy <vidigummy@gmail.com>
2021-01-29 20:43:59 +0900

    middle

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
+var bodyParser = require('body-parser');
 
 var connection = mysql.createConnection({
   host : 'localhost',
@@ -18,7 +19,43 @@ try{
 }
 
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 
+
+var sql = "CREATE TABLE book (name VARCHAR(20), email VARCHAR(55), phone VARCHAR(20))";
+
+var port = 3000; // 사용할 포트 번호를 port 변수에 넣습니다.
+
+app.listen(port, function(){ // port변수를 이용하여 3000번 포트에 node.js 서버를 연결합니다.
+  console.log('server on! http://localhost:'+port); //서버가 실행되면 콘솔창에 표시될 메세지입니다.
+});
+
+app.get('/', function(req, res){
+  res.redirect('/contacts');
+  console.log("hey");
+});
+// Contacts - Index // 7
+app.get('/contacts', function(req, res){
+  Contact.find({}, function(err, contacts){
+    if(err) return res.json(err);
+    res.render('contacts/index', {contacts:contacts});
+  });
+});
+// Contacts - New // 8
+app.get('/contacts/new', function(req, res){
+  res.render('contacts/new');
+  console.log("hey");
+});
+// Contacts - create // 9
+app.post('/contacts', function(req, res){
+  Contact.create(req.body, function(err, contact){
+    if(err) return res.json(err);
+    res.redirect('/contacts');
+  });
+});
 
 /*
 //테이블 조회하는 쿼리
